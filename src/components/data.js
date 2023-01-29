c978509e2237d596f7b8836d610db96d5b2e7d6c
codeCrisis09 <pruthvirajpasi676@gmail.com>
2023-01-29 10:14:15 +0530

    Made changes to the Description in data.js file

--- src/components/data.js
+++ src/components/data.js
@@ -5,7 +5,7 @@ export default [
         maps: "https://goo.gl/maps/WHA3iqfecscdkXG88",
         name: "Seoul Tower",
         date: "12'Jan,2021 - 24'Jan,2021",
-        description: "The N Seoul Tower, officially the YTN Seoul Tower and commonly known as Namsan Tower or Seoul Tower, is a communication and observation tower located on Nam Mountain in central Seoul, South Korea.The N Tower has four floors: 1F, 2F, 3F, and 5F (most buildings in Korea avoid having fourth floors).[10] There are four observation decks (the 4th observation deck, which is the revolving restaurant, rotates at a rate of one revolution every 48 minutes), as well as gift shops and two restaurants. Most of the city of Seoul can be seen from the top. Close to N Seoul Tower is a second lattice transmission tower. The tower offers a digital observatory with a 360° panoramic view that showcases Korea's history through 32 LCD screens. This is located on the third floor of the N Tower.[6]"
+        description: "The N Seoul Tower, officially the YTN Seoul Tower and commonly known as Namsan Tower or Seoul Tower, is a communication and observation tower located on Nam Mountain in central Seoul, South Korea.The N Tower has four floors: 1F, 2F, 3F, and 5F (most buildings in Korea avoid having fourth floors).[10] There are four observation decks (the 4th observation deck."
     },
     {
         img:"https://images.unsplash.com/photo-1672643305500-e4820e8b6db2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
@@ -13,7 +13,7 @@ export default [
         maps:"https://goo.gl/maps/JMAByndvdchoUZKw9",
         name:"Yokohoma Fireworks",
         date:"27'Feb,2022 - 13'March,2022",
-        description:"Yokohama, a Japanese city south of Tokyo, was one of the first Japanese ports opened to foreign trade, in 1859. It contains a large Chinatown with hundreds of Chinese restaurants and shops. It's also known for Sankei-en Garden. Yokohama (Japanese: 横浜, pronounced [jokohama] (listen)) is the second-largest city in Japan by population[1] and the most populous municipality of Japan. It is the capital city and the most populous city in Kanagawa Prefecture, with a 2020 population of 3.8 million. It lies on Tokyo Bay, south of Tokyo, in the Kantō region of the main island of Honshu. Yokohama is also the major economic, cultural, and commercial hub of the Greater Tokyo Area along the Keihin Industrial Zone."
+        description:"Yokohama, a Japanese city south of Tokyo, was one of the first Japanese ports opened to foreign trade, in 1859. It contains a large Chinatown with hundreds of Chinese restaurants and shops. It's also known for Sankei-en Garden. Yokohama (Japanese: 横浜, pronounced [jokohama] (listen)) is the second-largest city in Japan by population[1] and the most populous municipality of Japan. "
     },
     {
         img:"https://images.unsplash.com/photo-1674073132417-d4f858dd7503?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=372&q=80",
@@ -21,7 +21,7 @@ export default [
         maps:"https://goo.gl/maps/uKaGKepa6fvQPGGv9",
         name:"Southwest of Washington DC",
         date:"7'Oct,2022 - 27'Oct,2022",
-        description:"Sprawling from the National Mall down the Potomac, Southwest Washington includes the Jefferson and Roosevelt memorials beside the Tidal Basin, which is famous for its spring cherry blossoms. Washington, D.C., formally the District of Columbia, also known as Washington, the District, or D.C., is the capital city and federal district of the United States.[10] It is located on the east bank of the Potomac River, which forms its southwestern border with Virginia, and it shares a land border with Maryland on its other sides. The city was named for George Washington, a Founding Father, commanding general of the Continental Army during the Revolutionary War, and the first president of the United States,[11] and the district is named after Columbia, the female personification of the nation. Washington is the southern point of the Northeast megalopolis."
+        description:"Sprawling from the National Mall down the Potomac, Southwest Washington includes the Jefferson and Roosevelt memorials beside the Tidal Basin, which is famous for its spring cherry blossoms. Washington, D.C., formally the District of Columbia, also known as Washington, the District, or D.C., is the capital city and federal district of the United States.[10] It is located on the east bank of the Potomac River."
     },
     {
         img:"https://images.unsplash.com/photo-1673942012605-17df07c724fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
@@ -29,6 +29,6 @@ export default [
         maps:"https://goo.gl/maps/uKaGKepa6fvQPGGv9",
         name:"Abu Simbel Pyramids",
         date:"9'Jan,2023 - 19'Oct,2023",
-        description: "Egypt (Arabic: مصر Miṣr [mesˁr], Egyptian Arabic pronunciation: [mɑsˤr]), officially the Arab Republic of Egypt, is a transcontinental country spanning the northeast corner of Africa and southwest corner of Asia via a land bridge formed by the Sinai Peninsula. It is bordered by the Mediterranean Sea to the north, the Gaza Strip of Palestine and Israel to the northeast, the Red Sea to the east, Sudan to the south, and Libya to the west. The Gulf of Aqaba in the northeast separates Egypt from Jordan and Saudi Arabia. Cairo is the capital and largest city of Egypt, while Alexandria, the second-largest city, is an important industrial and tourist hub at the Mediterranean coast.[13] At approximately 100 million inhabitants, Egypt is the 14th-most populated country in the world.Egypt has one of the longest histories of any country, tracing its heritage along the Nile Delta back to the 6th–4th millennia BCE."
+        description: "Egypt (Arabic: مصر Miṣr [mesˁr], Egyptian Arabic pronunciation: [mɑsˤr]), officially the Arab Republic of Egypt, is a transcontinental country spanning the northeast corner of Africa and southwest corner of Asia via a land bridge formed by the Sinai Peninsula. It is bordered by the Mediterranean Sea to the north, the Gaza Strip of Palestine and Israel to the northeast, the Red Sea to the east, Sudan to the south, and Libya to the west. "
     }
 ]
